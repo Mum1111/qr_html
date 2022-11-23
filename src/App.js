5d849d4641c84743a086559b32e98ab70aaf17b5
Mumi <543528285@qq.com>
2022-11-23 10:40:48 +0800

    feat :二维码生成功能完成

--- src/App.js
+++ src/App.js
@@ -1,22 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import { Input, Radio,message } from "antd";
+import QRCode from "qrcode.react";
+import { useState } from "react";
+import pic1 from "./static/img/2221.png";
+import pic2 from "./static/img/2223.png";
+
+import "./App.css";
 
 function App() {
+  const [url, setUrl] = useState("");
+  const [size, setSize] = useState(512);
+  const [innerImgSize, setInnerImgSize] = useState(140);
+  const [value, setValue] = useState(1);
+  const [messageApi, contextHolder] = message.useMessage();
+
+  // let innerImgSize = 280;
+
+  const changeCanvasToPic = (imgSize) => {
+    setSize(imgSize);
+    setInnerImgSize(Math.floor(imgSize / 4))
+    // innerImgSize = Math.floor(imgSize / 3);
+
+    setTimeout(() => {
+      if(url === '') {
+        messageApi.open({
+          type: 'error',
+          content: 'URL 不能为空',
+        });
+        return
+      }
+      console.log(size, innerImgSize);
+      const fileName = `${new Date().getTime()}`;
+      const canvasImg = document.getElementById("qrCode"); // 获取canvas类型的二维码
+      const img = new Image();
+      const link = document.createElement("a");
+      const evt = document.createEvent("MouseEvents");
+      img.src = canvasImg.toDataURL("image/png"); // 将canvas对象转换为图片的data url
+      link.style.display = "none";
+      link.href = img.src;
+      link.download = fileName;
+      document.body.appendChild(link); // 此写法兼容可火狐浏览器
+      evt.initEvent("click", false, false);
+      link.dispatchEvent(evt);
+      document.body.removeChild(link);
+    }, 50);
+  };
+
+  const handleChange = (e) => {
+    setUrl(e.target.value);
+  };
+
+  const onRadioChange = (e) => {
+    setValue(e.target.value);
+  };
+
   return (
     <div className="App">
+      {contextHolder}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <QRCode
+          className="qr"
+          id="qrCode"
+          value={url}
+          size={size} // 二维码的大小
+          fgColor="#000000" // 二维码的颜色
+          style={{ marginBottom: "40px" }}
+          level="H"
+          imageSettings={{
+            // 二维码中间的logo图片
+            src: value === 1 ? pic1 : value === 2 ? pic2 : "",
+            height: innerImgSize,
+            width: innerImgSize,
+            // excavate: true, // 中间图片所在的位置是否镂空
+          }}
+        />
+        <Radio.Group
+          onChange={onRadioChange}
+          size="large"
+          buttonStyle="solid"
+          defaultValue={1}
+          value={value}
         >
-          Learn React
-        </a>
+          <Radio.Button value={1}>白底</Radio.Button>
+          <Radio.Button value={2}>蓝底</Radio.Button>
+          <Radio.Button value={3}>无底</Radio.Button>
+        </Radio.Group>
+        <p>
+          <Input.TextArea
+          // type="textarea"
+            className="input_len"
+            size="large"
+            placeholder="输入URL"
+            onChange={handleChange}
+          />
+        </p>
+        <div className="button_group">
+          <div className="App-link" onClick={() => changeCanvasToPic(512)}>
+            生成二维码(1024)
+          </div>
+          <div className="App-link" onClick={() => changeCanvasToPic(1024)}>
+            生成二维码(2048)
+          </div>
+        </div>
       </header>
     </div>
   );
